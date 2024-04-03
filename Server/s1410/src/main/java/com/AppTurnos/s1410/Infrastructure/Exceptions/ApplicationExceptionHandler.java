1a97a2e639ae5eecca4a74952da6da3fa5e2aa2e
Lucas <lucasvonelm@gmail.com>
2024-04-03 11:32:00 -0300

    Added Comments

--- Server/s1410/src/main/java/com/AppTurnos/s1410/Infrastructure/Exceptions/ApplicationExceptionHandler.java
+++ Server/s1410/src/main/java/com/AppTurnos/s1410/Infrastructure/Exceptions/ApplicationExceptionHandler.java
@@ -13,6 +13,9 @@ import java.time.ZonedDateTime;
 @RestControllerAdvice
 public class ApplicationExceptionHandler {
 
+    //Maneja las expeciocnes que corresponden con el error 404 Not Found
+    //Para Agregar una expeción a ser manejada así solo deben colocarla dentro de las llaves
+    //En caso de que hagan una expeción propia, solo necesita que haga "extends" de RuntimeException
     @ExceptionHandler(value = {EntityNotFoundException.class})
     public ResponseEntity<Object> handleNotFoundBusinessException(RuntimeException exception) {
 
@@ -26,10 +29,13 @@ public class ApplicationExceptionHandler {
         return new ResponseEntity<>(apiException, notFound);
     }
 
+
+    //TODO: Manejar Binding Result Exceptions
+
+    //Maneja cualquier otra excepción que no haya sido considerada en los casos anteriores
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<Object> handleAllOtherExceptions(Exception exception) {
         HttpStatus internalServerError = HttpStatus.INTERNAL_SERVER_ERROR;
-        exception.printStackTrace();
 
         ApiException apiException = new ApiException(
                 exception.getMessage(),
