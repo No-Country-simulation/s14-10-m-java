f12298f7a3c82ae308768282803d168bc24bb0c0
Juan Oscco <juan.osccomori@gmail.com>
2024-04-03 16:44:03 -0500

    refactor: Comment out unused modules in AppModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { NotFoundModule } from './modules/not-found/not-found.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { HomeModule } from './modules/home/home.module';
+import { SharedModule } from './core/shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,8 +16,9 @@ import { HomeModule } from './modules/home/home.module';
     AppRoutingModule,
     HttpClientModule,
     NotFoundModule,
-    AuthModule,
-    HomeModule
+    // AuthModule,
+    // HomeModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent],
