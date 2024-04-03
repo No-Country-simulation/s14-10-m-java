0a2fc395cec4ac6e26cff2a69b22fdf4700b9e00
FacundoLB <facuu142@gmail.com>
2024-04-03 14:07:53 -0300

    actualizando home

--- frontend/src/app/modules/home/home.module.ts
+++ frontend/src/app/modules/home/home.module.ts
@@ -3,14 +3,19 @@ import { CommonModule } from '@angular/common';
 import { PrincipalComponent } from './principal/principal.component';
 
 import { HomeRoutingModule} from './home-routing.module'
+import { CardComponent } from 'src/app/core/shared/components/card/card.component';
 
 @NgModule({
   declarations: [
-    PrincipalComponent
+    PrincipalComponent,
+    CardComponent
   ],
   imports: [
     CommonModule,
     HomeRoutingModule
+  ],
+  exports: [
+    CardComponent
   ]
 })
 export class HomeModule { }
