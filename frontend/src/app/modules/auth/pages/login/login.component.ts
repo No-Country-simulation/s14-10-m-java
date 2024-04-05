89f2ef6ff4c11f71a8f0587c53f6d97ae45f1db7
SwatColombia <fabetancur10@misena.edu.co>
2024-04-05 13:15:11 -0500

    login añadido falta css

--- frontend/src/app/modules/auth/pages/login/login.component.ts
+++ frontend/src/app/modules/auth/pages/login/login.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+//import { emailValidator } from '@utils/validator';
+//import Swal from 'sweetalert2';
+//import { AuthService } from '@services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  //loginForm: FormGroup;
+  //showPassword: boolean = false;
+  //error: string = 'Error en la aplicacion';
 
+  constructor(
+    //private readonly authService: AuthService,
+    //private readonly fb: FormBuilder,
+    private readonly router: Router
+  ) {}
 }
