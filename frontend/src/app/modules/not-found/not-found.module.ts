3a719d1dcffd032f77a80ec2a127dee04dc614d9
Elcira Ibarra <elcira@qubikdigital.com>
2024-04-05 03:48:26 -0300

    Chore: Added under construction page

--- frontend/src/app/modules/not-found/not-found.module.ts
+++ frontend/src/app/modules/not-found/not-found.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageUnderConstructionComponent } from './page-under-construction/page-under-construction.component';
 import { NotFoundRoutingModule } from './not-found-routing.module';
+import { SharedModule } from 'src/app/core/shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
-    PageUnderConstructionComponent
+    PageUnderConstructionComponent,
   ],
   imports: [
     CommonModule,
-    NotFoundRoutingModule
+    NotFoundRoutingModule,
+    SharedModule
   ]
 })
 export class NotFoundModule { }
