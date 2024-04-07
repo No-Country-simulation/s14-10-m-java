ac0cfdead19d6727b0e279abd759a902eeca09a1
Agustin <agustingomez22059898@gmail.com>
2024-04-07 13:38:41 -0300

    Actualizacion de rama newDoctor

--- Server/src/main/java/com/s1410/calme/Application/SImplementations/DoctorServiceImpl.java
+++ Server/src/main/java/com/s1410/calme/Application/SImplementations/DoctorServiceImpl.java
@@ -2,13 +2,16 @@ package com.s1410.calme.Application.SImplementations;
 import com.s1410.calme.Domain.Dtos.request.RequestCreateDoctor;
 import com.s1410.calme.Domain.Dtos.request.RequestEditDoctor;
 import com.s1410.calme.Domain.Dtos.response.ResponseDoctor;
+import com.s1410.calme.Domain.Entities.Doctor;
 import com.s1410.calme.Domain.Mapper.DoctorMapper;
 import com.s1410.calme.Domain.Repositories.DoctorRepository;
 import com.s1410.calme.Domain.Services.DoctorService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 @Service
@@ -22,16 +25,26 @@ public class DoctorServiceImpl implements DoctorService {
     @Transactional
     @Override
     public ResponseDoctor createDoctor(RequestCreateDoctor requestCreateDoctor) {
-        return null;
+
+    if (requestCreateDoctor == null){throw new EntityNotFoundException();}
+
+    Doctor doctor = doctorMapper.requestCreateToDoctor(requestCreateDoctor);
+    doctor.setActive(true);
+    doctorRepository.save(doctor);
+
+        return doctorMapper.doctorToResponse(doctor);
     }
 
     @Override
     public ResponseDoctor readDoctor(Long id) {
-        return null;
+
+        Doctor doctor = doctorRepository.findById(id).orElseThrow( ()->
+                new EntityNotFoundException("The doctor with id: "+ id+" was not found"));
+        return doctorMapper.doctorToResponse(doctor);
     }
 
     @Override
-    public List<ResponseDoctor> readAllDoctors() {
+    public List<ResponseDoctor> readAllDoctors(boolean active, Pageable paging) {
         return null;
     }
 
@@ -42,6 +55,12 @@ public class DoctorServiceImpl implements DoctorService {
 
     @Override
     public Boolean deleteDoctor(Long id) {
-        return null;
+
+        Doctor doctor = doctorRepository.findById(id).orElseThrow( ()->
+                new EntityNotFoundException("The doctor with id: "+ id+" was not found"));
+
+        doctor.setActive(!doctor.getActive());
+        doctorRepository.save(doctor);
+        return doctor.getActive();
     }
 }
--- Server/src/main/java/com/s1410/calme/Domain/Services/DoctorService.java
+++ Server/src/main/java/com/s1410/calme/Domain/Services/DoctorService.java
@@ -2,13 +2,17 @@ package com.s1410.calme.Domain.Services;
 import com.s1410.calme.Domain.Dtos.request.RequestCreateDoctor;
 import com.s1410.calme.Domain.Dtos.request.RequestEditDoctor;
 import com.s1410.calme.Domain.Dtos.response.ResponseDoctor;
+
+import java.awt.print.Pageable;
 import java.util.List;
 
 public interface DoctorService {
 
     ResponseDoctor createDoctor(RequestCreateDoctor requestCreateDoctor);
     ResponseDoctor readDoctor(Long id);
-    List<ResponseDoctor> readAllDoctors();
+
+    List<ResponseDoctor> readAllDoctors(boolean active, Pageable paging);
+
     ResponseDoctor updateDoctor(RequestEditDoctor requestEditDoctor);
     Boolean deleteDoctor(Long id);
 }
