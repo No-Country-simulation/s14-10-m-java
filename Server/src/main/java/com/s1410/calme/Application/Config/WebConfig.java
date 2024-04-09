4399ab6c9ee92a4766676330dc0a3779d2794649
nestorduqueduque <nestorduqueduque@gmail.com>
2024-04-09 10:56:01 -0500

    prueba 4

--- Server/src/main/java/com/s1410/calme/Application/Config/WebConfig.java
+++ Server/src/main/java/com/s1410/calme/Application/Config/WebConfig.java
@@ -1,13 +1,12 @@
-package com.s1410.calme.Application.Config;
-
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-public class WebConfig implements WebMvcConfigurer {
-    //Descomentar para que funcione el cors sin jwt
+//package com.s1410.calme.Application.Config;
+//
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//public class WebConfig implements WebMvcConfigurer {
 //    @Override
 //    public void addCorsMappings(CorsRegistry registry) {
 //        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE");
 //        ;
 //    }
-}
+//}
