cc16c99cb8e44a9f8940d9aca54c02dab68e120a
Juan Oscco <juan.osccomori@gmail.com>
2024-04-10 16:03:56 -0500

    feat: implementing registration types

--- frontend/src/app/modules/auth/auth.module.ts
+++ frontend/src/app/modules/auth/auth.module.ts
@@ -2,19 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { FormsModule } from '@angular/forms';
 
-import { AuthRoutingModule } from './auth-routing.module'
+import { AuthRoutingModule } from './auth-routing.module';
 import { SharedModule } from 'src/app/core/shared/shared.module';
+import { RegisterDoctorComponent } from './pages/register-doctor/register-doctor.component';
+import { RegisterPatientComponent } from './pages/register-patient/register-patient.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    RegisterDoctorComponent,
+    RegisterPatientComponent,
   ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule,
-    SharedModule
-  ]
+  imports: [CommonModule, AuthRoutingModule, SharedModule, FormsModule],
 })
-export class AuthModule { }
+export class AuthModule {}
