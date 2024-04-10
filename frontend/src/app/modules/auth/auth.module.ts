08d1f42559b2dc74ca57b97eb604440fe4a30f01
SwatColombia <fabetancur10@misena.edu.co>
2024-04-10 14:37:25 -0500

    add login wait ngrx-toast

--- frontend/src/app/modules/auth/auth.module.ts
+++ frontend/src/app/modules/auth/auth.module.ts
@@ -2,19 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AuthRoutingModule } from './auth-routing.module'
+import { AuthRoutingModule } from './auth-routing.module';
 import { SharedModule } from 'src/app/core/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule,
-    SharedModule
-  ]
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule, SharedModule],
 })
-export class AuthModule { }
+export class AuthModule {}
