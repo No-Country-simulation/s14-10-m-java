762452aec3c98a13fe3b1207ae01b61ff375ab11
Juan Oscco <juan.osccomori@gmail.com>
2024-04-12 09:54:17 -0500

    feat: modify register

--- frontend/src/app/modules/auth/pages/register-doctor/register-doctor.component.ts
+++ frontend/src/app/modules/auth/pages/register-doctor/register-doctor.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RegisterDoctorService } from "../../services/register-doctor.service";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-doctor',
@@ -19,7 +21,14 @@ export class RegisterDoctorComponent {
     password: '',
     specialty: '',
     availability: '',
+    phoneNumber: 0, // Agregado phoneNumber
+    morning: false, // Agregado morning
+    afternoon: false, // Agregado afternoon
+    night: false, // Agregado night
+    postalCode: 0, // Agregado postalCode
   };
+
+  constructor(private registerDoctorService: RegisterDoctorService, private router: Router) { }
 
   goToStep(step: number) {
     this.currentStep = step;
@@ -33,8 +42,6 @@ export class RegisterDoctorComponent {
 
   submitForm() {
     const formData = {
-      //  ...this.formDataStep1,
-      //  ...this.formDataStep2
       email: this.formDataStep2.email,
       password: this.formDataStep2.password,
       firstName: this.formDataStep1.firstName,
@@ -43,8 +50,33 @@ export class RegisterDoctorComponent {
       DNI: this.formDataStep1.DNI,
       specialty: this.formDataStep2.specialty,
       dateOfBirth: this.formDataStep1.dateOfBirth,
-      availability: this.formDataStep2.availability,
+      phoneNumber: Number(this.formDataStep2.phoneNumber), // Cambiado a phoneNumber
+      morning: this.formDataStep2.morning, // Cambiado a morning
+      afternoon: this.formDataStep2.afternoon, // Cambiado a afternoon
+      night: this.formDataStep2.night, // Cambiado a night
+      postalCode: Number(this.formDataStep2.postalCode), // Cambiado a postalCode
     };
     console.log(formData);
+    this.registerDoctorService.sendFormData(formData).subscribe(
+      (response) => {
+        console.log('Datos enviados correctamente:', response); this.router.navigateByUrl('/auth/login');
+      },
+      (error) => {
+        console.error('Error al enviar los datos:', error);
+      }
+    )
   }
 }
+
+/**
+ *
+ * this.registerDoctorService.sendFormData(formData).subscribe(
+      (response) => {
+        console.log('Datos enviados correctamente:', response);
+      },
+      (error) => {
+        console.error('Error al enviar los datos:', error);
+      }
+    )
+ *
+ */
