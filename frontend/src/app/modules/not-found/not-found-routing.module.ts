47930def5852e6aa6a749ca59dd62816c6ce5348
Elcira Ibarra <elcira@qubikdigital.com>
2024-04-13 03:44:33 -0300

    Implemented guard for route protection

--- frontend/src/app/modules/not-found/not-found-routing.module.ts
+++ frontend/src/app/modules/not-found/not-found-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageUnderConstructionComponent } from './page-under-construction/page-under-construction.component';
+import { AuthGuard } from 'src/app/core/shared/guards/auth.guard';
 
-const routes: Routes = [{
-  path: '',
-  children: [{ path: '', component: PageUnderConstructionComponent }],
-},]
+const routes: Routes = [
+  {
+    path: '',
+    children: [{
+      path: '',
+      component: PageUnderConstructionComponent
+    }],
+    canActivate: [AuthGuard]
+  }
+,]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
