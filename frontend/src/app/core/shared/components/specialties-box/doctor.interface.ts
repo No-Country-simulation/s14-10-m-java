85cd6f625b1d3f4d8940edb1312bdd400ea8afc5
FacundoLB <facuu142@gmail.com>
2024-04-15 14:12:09 -0300

    added function to filter by availability

--- frontend/src/app/core/shared/components/specialties-box/doctor.interface.ts
+++ frontend/src/app/core/shared/components/specialties-box/doctor.interface.ts
@@ -1,15 +1,15 @@
 export interface Doctor {
     email: string;
-    firstName: string | null;
-    secondName: string | null;
-    lastName: string | null;
+    firstName: string;
+    secondName: string;
+    lastName: string;
     DNI: string;
     specialty: string;
-    phoneNumber: number | null;
-    morning: boolean | null;
-    afternoon: boolean | null;
-    night: boolean | null;
-    postalCode: number | null;
-    licenseNumber: string | null;
-    address: string | null;
+    phoneNumber: number;
+    morning: boolean;
+    afternoon: boolean;
+    night: boolean;
+    postalCode: number;
+    licenseNumber: string;
+    address: string;
 }
--- frontend/src/app/modules/home/home.module.ts
+++ frontend/src/app/modules/home/home.module.ts
@@ -11,9 +11,11 @@ import { SpecialtiesBoxComponent } from 'src/app/core/shared/components/specialt
 import { SearchDoctorComponent } from './search-doctor/search-doctor.component';
 import { DoctorSearchCardComponent } from 'src/app/core/shared/components/doctor-search-card/doctor-search-card.component';
 import { SearchFiltersComponent } from 'src/app/core/shared/components/search-filters/search-filters.component';
+import { AppointmentConfirmationComponent } from './appointment-confirmation/appointment-confirmation.component';
+import { AppointmentComponent } from 'src/app/core/shared/components/appointment/appointment.component';
 
 @NgModule({
-  declarations: [PrincipalComponent, CardComponent, SliderComponent,SpecialtiesBoxComponent, SearchDoctorComponent,DoctorSearchCardComponent,SearchFiltersComponent],
+  declarations: [PrincipalComponent, CardComponent, SliderComponent,SpecialtiesBoxComponent, SearchDoctorComponent,DoctorSearchCardComponent,SearchFiltersComponent, AppointmentConfirmationComponent,AppointmentComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule, SearchBarModule],
   exports: [CardComponent],
 })
